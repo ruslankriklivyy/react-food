950ccf0d4d2cc6aa38a46e5a2fcb1e4db2542d5a
ruslankriklivyy <ruslankriklivyy@gmail.com>
2021-04-04 10:53:16 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import { Header } from './components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: 'Karla', sans-serif;
+  }
+  html, body {
+      margin: 0;
+      width: 100%;
+      padding: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyle>
+      <Header />
+    </GlobalStyle>
   );
 }
 
