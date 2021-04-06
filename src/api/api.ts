21672465ced708703b662b87bde48ef297281065
ruslankriklivyy <ruslankriklivyy@gmail.com>
2021-04-06 21:36:49 +0300

    Fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import { FoodStoreType } from '../store/CategoryStore';
 
-// const instance = axios.create({
-//   baseURL: 'http://localhost:3001/',
-// });
+const instance = axios.create({
+  baseURL: 'http://localhost:3001/',
+});
 
 export const fetchCategories = (): Promise<any> => {
-  return axios.get('categories').then(({ data }) => data);
+  return instance.get('categories').then(({ data }) => data);
 };
 
 export const fetchFood = (id: number, name: string): Promise<any> => {
-  return axios.get(`food${id !== null ? `?categoryId=${id}` : ''}`).then(({ data }) => {
+  return instance.get(`food${id !== null ? `?categoryId=${id}` : ''}`).then(({ data }) => {
     const newData = data.filter(
       (item: FoodStoreType) => item.name.toLowerCase().indexOf(name.toLowerCase()) >= 0,
     );
